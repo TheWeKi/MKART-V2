abeea7fa9cc53ea4ce52f817db63798d910f6e6f
Rituraj Pandey <rp.weki.24@gmail.com>
2023-09-17 17:21:25 +0530

    Product Detail Link Added on ProductCard in ProductList

--- client/src/components/product_list/ProductLayout.tsx
+++ client/src/components/product_list/ProductLayout.tsx
@@ -1,34 +1,22 @@
 import ProductCard from "./ProductCard"
+import {Link} from "react-router-dom";
+
+const productIds = [1, 2,3 , 4, 5, 6, 7, 8, 9];
 
 const ProductLayout = () => {
     return (
         <>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12 justify-items-center p-12">
-                <div>
-                    <ProductCard/>
-                </div>
-                <div>
-                    <ProductCard/>
-                </div>
-                <div>
-                    <ProductCard/>
-                </div>
-                <div>
-                    <ProductCard/>
-                </div>
 
-                <div>
-                    <ProductCard/>
-                </div>
-                <div>
-                    <ProductCard/>
-                </div>
-                <div>
-                    <ProductCard/>
-                </div>
-                <div>
-                    <ProductCard/>
-                </div>
+                {
+                    productIds.map(id => (
+                        <div key={id}>
+                            <Link to='/productdetail'>
+                                <ProductCard/>
+                            </Link>
+                        </div>
+                    ))
+                }
             </div>
         </>
     )
