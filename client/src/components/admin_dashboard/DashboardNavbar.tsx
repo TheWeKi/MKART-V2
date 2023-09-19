993ca57d9dee2f977e6231230846f9147d99ae5b
Shekhar Kumar <kshekhar2807@gmail.com>
2023-09-19 13:45:22 +0530

    Indentation

--- client/src/components/admin_dashboard/DashboardNavbar.tsx
+++ client/src/components/admin_dashboard/DashboardNavbar.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import {useState} from "react";
 import Divider from "../ui/Divider";
 
 const DashboardNavbar = () => {
@@ -10,21 +10,21 @@ const DashboardNavbar = () => {
             <div className="flex justify-evenly bg-base-100 items-center max-h-[1vh]">
                 <div>
                     <button className="link link-hover text-lg font-semibold"
-                        onClick={() => setProductMenu(true)}
+                            onClick={() => setProductMenu(true)}
                     >
                         Products
                     </button>
                 </div>
                 <div>
                     <button className="link link-hover text-lg font-semibold"
-                        onClick={() => setProductMenu(false)}
+                            onClick={() => setProductMenu(false)}
                     >
                         Users
                     </button>
                 </div>
                 <div>
                     <button className="link link-hover text-lg font-semibold"
-                        onClick={() => setProductMenu(false)}
+                            onClick={() => setProductMenu(false)}
                     >
                         Orders
                     </button>
@@ -35,19 +35,22 @@ const DashboardNavbar = () => {
 
             <div className="flex justify-between bg-base-100 items-center max-h-[1vh] px-[8rem]">
                 <div className="flex-1">
-                {
-                    productMenu 
-                        && 
-                    <button className="btn btn-outline btn-sm">
-                        Create New Product
-                    </button>
-                }
+                    {
+                        productMenu
+                        &&
+                        <button className="btn btn-outline btn-sm">
+                            Create New Product
+                        </button>
+                    }
                 </div>
                 <div className="flex gap-4">
-                    <input type="text" placeholder={"Search By ID"} className="input input-bordered border-base-600 w-[24rem]"/>
+                    <input type="text" placeholder={"Search By ID"}
+                           className="input input-bordered border-base-600 w-[24rem]"/>
                     <button className="btn btn-ghost btn-circle btn-outline border-base-300">
-                        <svg className="w-8 h-8 fill-current" fill="none" stroke="currentColor" strokeWidth={2} viewBox="0 0 24 24" aria-hidden="true">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75" />
+                        <svg className="w-8 h-8 fill-current" fill="none" stroke="currentColor" strokeWidth={2}
+                             viewBox="0 0 24 24" aria-hidden="true">
+                            <path strokeLinecap="round" strokeLinejoin="round"
+                                  d="M4.5 12h15m0 0l-6.75-6.75M19.5 12l-6.75 6.75"/>
                         </svg>
                     </button>
                 </div>
--- client/src/components/admin_dashboard/DashboardTable.tsx
+++ client/src/components/admin_dashboard/DashboardTable.tsx
@@ -9,45 +9,47 @@ const DashboardTable = () => {
                 <table className="table table-zebra table-pin-rows table-lg">
                     {/* head */}
                     <thead>
-                        <tr>
-                            <th>Name</th>
-                            <th>Job</th>
-                            <th>Favorite Color</th>
-                            <th></th>
-                        </tr>
+                    <tr>
+                        <th>Name</th>
+                        <th>Job</th>
+                        <th>Favorite Color</th>
+                        <th></th>
+                    </tr>
                     </thead>
 
 
                     <tbody>
 
-                        {
-                            id.map(i => (
-                                <tr key={i}>
-                                    <td>
-                                        <div className="flex items-center space-x-3">
-                                            <div className="avatar">
-                                                <div className="mask mask-squircle w-12 h-12">
-                                                    <img src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Avatar Tailwind CSS Component" />
-                                                </div>
-                                            </div>
-                                            <div>
-                                                <div className="font-bold">Hart Hagerty</div>
-                                                <div className="text-sm opacity-50">United States</div>
+                    {
+                        id.map(i => (
+                            <tr key={i}>
+                                <td>
+                                    <div className="flex items-center space-x-3">
+                                        <div className="avatar">
+                                            <div className="mask mask-squircle w-12 h-12">
+                                                <img
+                                                    src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+                                                    alt="Avatar Tailwind CSS Component"/>
                                             </div>
                                         </div>
-                                    </td>
-                                    <td>
-                                        Zemlak, Daniel and Leannon
-                                        <br />
-                                        <span className="badge badge-ghost badge-sm">Desktop Support Technician</span>
-                                    </td>
-                                    <td>Purple</td>
-                                    <th>
-                                        <button className="btn btn-ghost btn-xs">details</button>
-                                    </th>
-                                </tr>
-                            ))
-                        }
+                                        <div>
+                                            <div className="font-bold">Hart Hagerty</div>
+                                            <div className="text-sm opacity-50">United States</div>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td>
+                                    Zemlak, Daniel and Leannon
+                                    <br/>
+                                    <span className="badge badge-ghost badge-sm">Desktop Support Technician</span>
+                                </td>
+                                <td>Purple</td>
+                                <th>
+                                    <button className="btn btn-ghost btn-xs">details</button>
+                                </th>
+                            </tr>
+                        ))
+                    }
 
                     </tbody>
 
