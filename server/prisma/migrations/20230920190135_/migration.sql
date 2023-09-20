7a8bec6e238cde9d1158de53e9fec14a5bccbbe9
Rituraj Pandey <rp.weki.24@gmail.com>
2023-09-21 00:59:19 +0530

    added Prisma Migration Data to gitignore file

---
+++ server/prisma/migrations/20230920190135_/migration.sql
@@ -0,0 +1,48 @@
+/*
+  Warnings:
+
+  - Added the required column `category` to the `Product` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `company` to the `Product` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `image` to the `Product` table without a default value. This is not possible if the table is not empty.
+  - Changed the type of `price` on the `Product` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
+
+*/
+-- AlterTable
+ALTER TABLE "Product" ADD COLUMN     "category" TEXT NOT NULL,
+ADD COLUMN     "company" TEXT NOT NULL,
+ADD COLUMN     "image" TEXT NOT NULL,
+DROP COLUMN "price",
+ADD COLUMN     "price" DOUBLE PRECISION NOT NULL;
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "roleAdmin" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Order" (
+    "id" TEXT NOT NULL,
+    "deliveryAddress" TEXT NOT NULL,
+    "isDelivered" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Order_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Cart" (
+    "id" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Cart_pkey" PRIMARY KEY ("id")
+);
