9d07e7a5cdcf2970a26964160585e655300f8a33
Rituraj Pandey <rp.weki.24@gmail.com>
2023-09-29 16:44:47 +0530

    Added RSA Keys

---
+++ server/prisma/migrations/20230929105130_init/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "OrderItem" (
+    "id" TEXT NOT NULL,
+    "cart" JSONB NOT NULL,
+    "orderId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "OrderItem_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "OrderItem" ADD CONSTRAINT "OrderItem_orderId_fkey" FOREIGN KEY ("orderId") REFERENCES "Order"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
