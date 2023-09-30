bc32280d3298c9065b48f80851dba39080dc85a2
Shekhar Kumar <kshekhar2807@gmail.com>
2023-09-30 19:03:43 +0530

    Bcrypt

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -1,15 +1,36 @@
 import prisma from "../database/prismaClient.js";
 import {dispatchJsonToken} from "../utils/dispatchToken.js";
 import Errorhandler from "../utils/errorhandler.js";
+import bcrypt from "bcryptjs";
 
 export const signUp = async (req, res, next) => {
     try {
+        const {email} = req.body;
+        const {username}=req.body;
+        const {password}=req.body;
+        const existingUser = await prisma.user.findFirst({
+            where: {
+                email: email,
+            }
+        }
+        );
+        if (existingUser) {
+            return next(new Errorhandler(400, "User Already Exists"));
+        }
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+        const data = {
+            email: email,
+            username:username,
+            password: hashedPassword,
+        }
         const user = await prisma.user.create({
-            data: req.body,
+            data: data,
         });
-        res
-            .status(201)
-            .json(user)
+
+        dispatchJsonToken(user, 201, res);
+
+
     } catch (e) {
         next(e);
     }
@@ -21,7 +42,7 @@ export const login = async (req, res, next) => {
         if (!user) {
             return next(new Errorhandler(404, "User Not Found"));
         }
-        const isMatch = password === user.password;
+        const isMatch = await bcrypt.compare(password, user.password);
         if (!isMatch) {
             return next(new Errorhandler(400, "Invalid Credentials"))
         }
