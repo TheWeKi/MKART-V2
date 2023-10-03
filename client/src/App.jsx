0caa12005ee82bcb75707737367d11c214552c33
Shekhar Kumar <kshekhar2807@gmail.com>
2023-10-03 22:50:50 +0530

    HiHi

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -29,7 +29,7 @@ const App = () => {
 
                     <Route path="/admin-dashboard/:dashboard" element={<AdminDashboard/>}/>
                     <Route path="/admin-dashboard/products/create-product" element={<CreateProduct/>}/>
-
+//
                     <Route path="*" element={<ErrorPage/>}/>
                 </Routes>
 
