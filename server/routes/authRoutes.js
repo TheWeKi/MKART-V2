141853d3aa5c60041983c0957763f43e272c064f
Rituraj Pandey <rp.weki.24@gmail.com>
2024-02-06 17:17:43 +0530

    Added Basic OAuth using passport

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,5 +1,7 @@
 import express from "express";
 import {login, signUp ,reset, newPassword} from "../controller/authController.js";
+import passport from "passport";
+import "../utils/google.js";
 
 
 const authRouter = express.Router();
@@ -20,5 +22,17 @@ authRouter
 authRouter
     .route('/newPassword')
     .post(newPassword);
+
+authRouter.route('/auth/google').get(passport.authenticate('google'), (req, res) => {
+    res.send('Google Auth');
+});
+
+authRouter.route('/auth/google/callback')
+    .get(
+        passport.authenticate('google', { failureRedirect: '/login' }),
+        (req, res) => {
+            res.redirect('http://localhost:3000');
+        });
+
 
 export default authRouter;
---
+++ server/utils/google.js
@@ -0,0 +1,47 @@
+import passport from "passport"
+import {Strategy as GoogleStrategy} from "passport-google-oauth20"
+
+import prisma from "../database/prismaClient.js";
+
+passport.use(
+    new GoogleStrategy({
+            clientID: process.env.GOOGLE_CLIENT_ID,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            callbackURL: process.env.GOOGLE_CALLBACK_URL,
+            scope: ["email", "profile"],
+        }, async (accessToken, refreshToken, profile, done) => {
+            try {
+                const user = await prisma.user.findUnique({
+                    where: {
+                        email: profile.emails[0].value,
+                    },
+                });
+
+                if (user) {
+                    return done(null, user);
+                }
+
+                const newUser = await prisma.user.create({
+                    data: {
+                        email: profile.emails[0].value,
+                        username: profile.displayName,
+                        id: profile.id,
+                        password: profile.id,
+                    },
+                });
+
+                return done(null, newUser);
+            } catch (error) {
+                return done(error, null);
+            }
+        }
+    )
+);
+
+passport.serializeUser((user, done) => {
+    done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+    done(null, user);
+});
