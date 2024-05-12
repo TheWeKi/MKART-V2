66e06f84e1ad47198910e192a1ac72daa7369bef
shekhar389 <kshekhar2807@gmail.com>
2024-05-12 19:32:59 +0530

    FIX

--- client/src/axios/baseUrl.js
+++ client/src/axios/baseUrl.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export const baseUrl = axios.create({
-    baseURL: "https://mkart-v2-server.onrender.com/api/v1",
+    baseURL: "/api/v1",
 })
--- server/app.js
+++ server/app.js
@@ -7,13 +7,10 @@ import cookieParser from "cookie-parser";
 import { errorMiddleware } from "./middleware/error.js";
 import orderRouter from "./routes/orderRoutes.js";
 import paymentRouter from "./routes/paymentRoutes.js";
-import cors from "cors";
 
 
 
 const app = express();
-
-app.use(cors({ origin: process.env.CLIENT_URL, credentials: true}));
 app.use(json());
 app.use(cookieParser());
 app.use(urlencoded({ extended: true }));
